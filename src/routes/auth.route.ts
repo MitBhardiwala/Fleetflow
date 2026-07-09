9293818ad151db1dde7fc9858dd5dfc6b03daac7
Mit0921 <meetbhardiwala@gmail.com>
2026-07-09 13:22:13 +0530

    fix: Format code for better readability in auth routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import { validate } from "../middleware/validate";
 import { forgotPasswordSchema, loginUserSchema } from "../utils/validations";
-import { login,forgotPassword } from "../controllers/auth.controller";
+import { login, forgotPassword } from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/login", validate({ body: loginUserSchema }), login);
-router.post("/forgot-password", validate({ body: forgotPasswordSchema }), forgotPassword);
+router.post(
+  "/forgot-password",
+  validate({ body: forgotPasswordSchema }),
+  forgotPassword,
+);
 // TODO create verify password api
 export default router;
